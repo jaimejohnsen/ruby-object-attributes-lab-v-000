4ca8a9580a6af06b095f7d9a8a1f66cbec2d7d88
Jaime Johnsen <drivetraindiva@gmail.com>
2019-03-06 20:51:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,51 @@
 class Dog
- 
   def name
-    @name
+    @name 
   end
   
-  def name= (dog_name)
+  def name=(dog_name)
     @name = dog_name
   end
-  
+
   def breed
     @breed
-    end
-    
-    def breed= (dog_breed)
-      @breed = dog_breed
-    end
   end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+#class Dog
+ 
+ # def name
+  #  @name
+#  end
+  
+ # def name= (dog_name)
+#    @name = dog_name
+ # end
+  
+#  def breed
+ #   @breed
+  #  end
+    
+   # def breed= (dog_breed)
+    #  @breed = dog_breed
+    #end
+  #end
 
   
   
--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,40 @@
 class Person
-
-def name 
-  @name
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
 end
 
-def name= (name)
-  @name = name
-end
 
-def job
-  @job
-end
 
-def job= (job)
-  @job = job
-end
-end
+
+
+#class Person
+
+#def name 
+#  @name
+#end
+
+#def name= (name)
+ # @name = name
+#end
+
+#def job
+#  @job
+#end
+
+#def job= (job)
+#  @job = job
+#end
+#end
